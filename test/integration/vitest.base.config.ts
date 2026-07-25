6c162bed00832f4fb1e34606786a75b139345de8
Marc Zwart <marcdevin@hotmail.com>
2026-07-25 15:35:26 +0200

    condense CI jobs, reduce fingerprint false testing to some smoke testing only

--- test/integration/vitest.base.config.ts
+++ test/integration/vitest.base.config.ts
@@ -5,18 +5,19 @@ import {
   readPlatform,
   readFingerprint,
   readBuildMode,
+  readServeMode,
 } from './test-matrix-parameters';
 
 const bundler = readBundler();
 const platform = readPlatform();
 const fingerprint = readFingerprint();
 const buildMode = readBuildMode();
-
+const serveMode = readServeMode();
 export function createVitestConfig(
   include: string[] | undefined,
   type: 'integration' | 'e2e-node',
 ) {
-  const configName = `${type}-${bundler}-${platform}-${fingerprint}-${buildMode}-${process.platform}`;
+  const configName = `type-${type}_bundler-${bundler}_platform-${platform}_serve-${serveMode}_fingerprint-${fingerprint}_build-${buildMode}_platform-${process.platform}`;
   const subdir = type === 'integration' ? 'integration' : 'e2e';
   return defineConfig({
     test: {
