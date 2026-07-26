ebd6019535a23d80341e8deaad21e5e3ab0557db
Marc Zwart <marcdevin@hotmail.com>
2026-07-26 15:29:17 +0200

    deprecate old plugin name

--- unplugin-dotnet-wasm/src/vite.ts
+++ unplugin-dotnet-wasm/src/vite.ts
@@ -3,3 +3,8 @@ import { dotnetWasmUnplugin } from './unplugin/index';
 const DotnetWasm = dotnetWasmUnplugin.vite;
 export default DotnetWasm;
 export { DotnetWasm as 'module.exports' };
+
+/**
+ * @deprecated Use `DotnetWasm` instead. `DotnetAssets` will be removed in a future release.
+ */
+export const DotnetAssets = DotnetWasm;
